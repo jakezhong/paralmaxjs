45e1323103fd403f41ee59b2c8357c35fd74bf30
Jake Zhong <jake@jumpingjackrabbit.com>
2018-02-26 17:57:58 -0500

    v1.1.8
    - update auto resize value

--- js/paralmax-min.js
+++ js/paralmax-min.js
@@ -1,1 +1,18 @@
-function paralmax(t){var e;return $(window).width(),e=$(window).height(),t.each(function(){function t(){$(window).width()>n.breakpoint?r.data("speed")||0===r.data("speed")?n.speed=r.data("speed"):n.speed=.5:n.speed=0}function a(){var t=r,e=$("html").scrollTop(),a=$("body").scrollTop(),s=(e>a?e:a)-n.start,o=parseInt(s*n.speed)+n.offset;t.css({webkitTransform:"translate3d(0, "+o+"px, 0)",MozTransform:"translate3d(0, "+o+"px, 0)",msTransform:"translateY("+o+"px)",OTransform:"translate3d(0, "+o+"px, 0)",transform:"translate3d(0, "+o+"px, 0)"})}var s,r=$(this),n={start:(s=r.offset()).top,stop:s.top+r.outerHeight(),speed:0,resizable:!!r.data("resize")&&r.data("resize"),breakpoint:r.data("breakpoint")?r.data("breakpoint"):0,offset:r.data("offset")?r.data("offset")*r.outerHeight():0};t(),function(){var t,a=r;!0===n.resizable&&(t=parseInt($(a).parent().offset().top)>parseInt(e)?parseInt(e*n.speed):parseInt($(a).parent().offset().top)*n.speed,a.css("height","+="+t))}(),a(),$(window).bind("scroll",function(){a()}),$(window).bind("resize",function(){t()})})}
+/* ========================================================================= */
+/* http://www.jksakura.com/paralmax */
+/**
+ * Paralmax is a JQuery responsive parallax plugin for multiple uses, including full size section background image, page parallax elements and more based on different use cases. With many built-in settings, users can set parallax speed, resize background image to full size or not, disable it below specific screen size, offset of the parallax element from it's center. The advantage of Paralmax is that it only adds parallax effet to elements, but not changes their style/looking, so users can easily customise/style any elements with pure HTML or CSS (position, size, image, background, content, etc).
+ * @author Jake Zhong
+----------------------  
+-------------| |   / /
+     | |     | |  / /
+     | |     | | / /
+     | |     | |/ /
+     | |     | |\ \
+     | |     | | \ \
+     | |     | |  \ \
+-----  |     | |   \ \
+-----------------------
+**/
+/* ========================================================================= */	 
+function paralmax(t){var e;return $(window).width(),e=$(window).height(),t.each(function(){function t(){$(window).width()>o.breakpoint?n.data("speed")||0===n.data("speed")?o.speed=n.data("speed"):o.speed=.5:o.speed=0}function a(){var t,a=n;!0===o.resizable&&(normalHeight=parseInt($(a).parent().outerHeight()),t=parseInt($(a).parent().offset().top)>parseInt(e)?parseInt(e*o.speed):parseInt($(a).parent().offset().top)*o.speed,a.css("height",normalHeight+t))}function r(){var t=n,e=$("html").scrollTop(),a=$("body").scrollTop(),r=(e>a?e:a)-o.start,s=parseInt(r*o.speed)+o.offset;t.css({webkitTransform:"translate3d(0, "+s+"px, 0)",MozTransform:"translate3d(0, "+s+"px, 0)",msTransform:"translateY("+s+"px)",OTransform:"translate3d(0, "+s+"px, 0)",transform:"translate3d(0, "+s+"px, 0)"})}var s,n=$(this),o={start:(s=n.offset()).top,stop:s.top+n.outerHeight(),speed:0,resizable:!!n.data("resize")&&n.data("resize"),breakpoint:n.data("breakpoint")?n.data("breakpoint"):0,offset:n.data("offset")?n.data("offset")*n.outerHeight():0};t(),a(),r(),$(window).bind("scroll",function(){r()}),$(window).bind("resize",function(){t(),a()})})}
